1a5cdf4a4ce6e16fec04290b3ce126e7509e1fb2
Rokaia Essam <rokaiaessam419@ci.suez.edu.eg>
2021-06-08 04:41:42 +0200

    fix deploy

--- Routers/Comment.js
+++ Routers/Comment.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const mongoose = require('mongoose');
 
-const Posts = require('../models/Posts');
+const Posts = require('../Models/Posts');
 
 const CommentRouter = express.Router();
 
